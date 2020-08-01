6b1dd1bb35807dd547bc6587085a9e9a023a1268
sahilKamble <abg1203@gmail.com>
2020-08-01 23:58:00 +0530

    test html

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const Students = require('../../models/subjects');
-const Subjects = require('../../models/subjects');
-
-Subjects.find({})
-.then(students => console.log(students));
+var subjectId ="" ;
+var div = "";
+var attendees = ['sebin francis','paras','sahil2','kapse'];
+const url  = "https://attn-server.herokuapp.com/";
+var url_subj = url+"subjects?name="+subject;
+let request1 = new X();
+request1.open("GET", url_subj);
+request1.send();
+request1.onload = () => {
+    if(request1.status === 200){
+        var jsonObj1 = JSON.parse(request1.responseText);
+        console.log(jsonObj1);
+        subjectId = jsonObj1[0]._id;
+        console.log(subjectId);
+        var url_subjectStudents = url+"subjects/"+subjectId+"/students";
+        console.log(url_subjectStudents); 
+    }
+}
