c89d93b80c5712b8bff117c07deeaca160614cc1
sahilKamble <abg1203@gmail.com>
2020-08-03 08:35:31 +0530

    index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,14 +19,28 @@
 //         console.log('test');
 //     }
 
-const url  = "https://attn-server.herokuapp.com/attn/table";
-const body = {"subid": "5f206d58ea613a00172d89ff"}
+const url  = "https://attn-server.herokuapp.com/attn/table/5f206d58ea613a00172d89ff";
+
 async function req() {       
-    let req = await fetch(url, {
-        method: 'GET',
-        body: JSON.stringify(body)
-    });
+    let req = await fetch(url);
     let res = await req.json();
-    console.log(res);
+    var table = '';
+    console.log(res.students.length);
+    var rows = res.students.length;
+    for(var r = -1; r < rows; r++) {
+        table += '<tr>';
+        for(var c = -2 ; c < res.dates.length; c++) {
+            if(r === -1 && c === -1) table += '<td>' + " " + '</td>';
+            else if(r === -1 ) {
+                var d = new Date(res.dates[c])
+                table += '<td>' + d.toLocaleString() + '</td>'
+            }
+            else if(c === -2) table += '<td>' + res.students[r].roll + '</td>';
+            else if(c === -1) table += '<td>' + res.students[r].name + '</td>';
+            else table += '<td>' + res.students[r].attn[c] + '</td>';
+        }
+        table += '</tr>';
+    }
+    document.write('<table border=1>' + table + '</table>');
 }
 req();
