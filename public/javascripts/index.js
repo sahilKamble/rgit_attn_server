d6cc67786af11d8301e0492e610c6fa65a35bd87
sahilKamble <abg1203@gmail.com>
2020-08-10 00:03:56 +0530

    changes to index page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,10 @@
-const url = "https://attn-server.herokuapp.com/attn/table/5f206d58ea613a00172d89ff";
-const attnurl = "https://attn-server.herokuapp.com/attn/sub/";
-const studentsurl = "https://attn-server.herokuapp.com/subjects/";
-var stop = false;
+// const url = "https://attn-server.herokuapp.com/attn/table/5f206d58ea613a00172d89ff";
+// const attnurl = "https://attn-server.herokuapp.com/attn/sub/";
+// const studentsurl = "https://attn-server.herokuapp.com/subjects/";
+
 var subjects = {};
-tableHeader = document.querySelector(".table-header");
-table = document.querySelector(".table-body");
+var tableHeader = document.querySelector(".table-header");
+var tableBody = document.querySelector(".table-body");
 let request1 = new XMLHttpRequest();
 request1.open("GET", "https://attn-server.herokuapp.com/subjects");
 request1.send();
@@ -17,8 +17,8 @@ request1.onload = () => {
         for (subject of jsonObj) {
             subjects[subject.name] = subject._id;
             var option = document.createElement("option");
-            option.classList.add("item")
-            option.setAttribute("value", "item-" + i++)
+            option.classList.add("item");
+            option.setAttribute("value", "item-" + i++);
             var node = document.createTextNode(subject.name);
             option.appendChild(node);
             var element = document.getElementById("subjects");
@@ -26,11 +26,6 @@ request1.onload = () => {
         }
         console.log(subjects);
     }
-}
-
-function cancel() {
-    stop = true;
-    document.querySelector(".show-attendance").disabled = false;
 }
 
 function toTitleCase(str) {
@@ -46,8 +41,7 @@ async function req(sid) {
     const url2 = "https://attn-server.herokuapp.com/attn/table/" + sid;
 
     document.querySelector(".show-attendance").disabled = true;
-    tableHeader = document.querySelector(".table-header");
-    table = document.querySelector(".table-body");
+    console.log(tableHeader);
     // console.log(studentsurl + sid + "/students");
     // let resp = await fetch(studentsurl + sid + "/students");
     // let data = await resp.json();
@@ -74,77 +68,87 @@ async function req(sid) {
         tableRoll.className = "col attn";
         let d = new Date(attn.date);
         // console.log(d);
-        tableRoll.innerHTML = d.toLocaleString();
+        tableRoll.innerHTML = d.toLocaleString('en-US', {
+            timeStyle: "short",
+            dateStyle: "short"
+        });
         tableHeader.appendChild(tableRoll);
     }
     let lect = data[0].attn.length;
     let tableTotal = document.createElement("th");
     tableTotal.className = "col attn";
-    tableTotal.innerHTML = "TotaL/" + lect;
+    tableTotal.innerHTML = "Total/" + lect;
     tableHeader.appendChild(tableTotal);
 
     for (student_info of data) {
-        if (!stop) {
-            const roll = student_info.student.roll;
-            const name = student_info.student.name;
-            // const id = student._id;
-            // let res = await fetch(attnurl + sid + "/" + id);
-            // let attns = await res.json();
-            // // console.log(attns)
-            let entry = document.createElement("tr");
-            entry.className = "table-row";
-            let tableName = document.createElement("td");
-            tableName.className = "col name";
-            tableName.innerHTML = toTitleCase(name);
-            entry.appendChild(tableName);
-            let tableRoll = document.createElement("td");
-            tableRoll.className = "col";
-            tableRoll.innerHTML = roll;
-            entry.appendChild(tableRoll);
-            // document.write(roll+ "  " + name + "  ");
-            var count = 0;
-            for (attn of student_info.attn) {
+        const roll = student_info.student.roll;
+        const name = student_info.student.name;
+        // const id = student._id;
+        // let res = await fetch(attnurl + sid + "/" + id);
+        // let attns = await res.json();
+        // // console.log(attns)
+        let entry = document.createElement("tr");
+        entry.className = "table-row";
+        let tableName = document.createElement("td");
+        tableName.className = "col name";
+        tableName.innerHTML = toTitleCase(name);
+        entry.appendChild(tableName);
+        let tableRoll = document.createElement("td");
+        tableRoll.className = "col";
+        tableRoll.innerHTML = roll;
+        entry.appendChild(tableRoll);
+        // document.write(roll+ "  " + name + "  ");
+        var count = 0;
+        for (attn of student_info.attn) {
 
-                // console.log(attn.date)
-                const s = attn.present ? "P" : "A";
-                if (attn.present) {
-                    count++;
-                }
-                let tableAttn = document.createElement("td");
-                tableAttn.className = "col";
-                tableAttn.innerHTML = s;
-                entry.appendChild(tableAttn);
-                // document.write(s + ' ');
-
+            // console.log(attn.date)
+            const s = attn.present ? "P" : "A";
+            if (attn.present) {
+                count++;
             }
             let tableAttn = document.createElement("td");
             tableAttn.className = "col";
-            tableAttn.innerHTML = count;
+            tableAttn.innerHTML = s;
             entry.appendChild(tableAttn);
-            table.appendChild(entry);
-            // document.write('<br/>');
-        } else {
-            return;
+            // document.write(s + ' ');
+
         }
+        let tableAttn = document.createElement("td");
+        tableAttn.className = "col";
+        tableAttn.innerHTML = count;
+        entry.appendChild(tableAttn);
+        tableBody.appendChild(entry);
+        // document.write('<br/>');
     }
     document.querySelector(".show-attendance").disabled = false;
 }
 
-// booton = document.querySelector('.show-attendance');
-// booton.onClick
-
-async function show() {
-    stop = false;
+function show() {
     tableHeader.innerHTML = '';
-    table.innerHTML = '';
+    tableBody.innerHTML = '';
     var sub = document.querySelector('#subjects');
     var subject = sub.options[sub.selectedIndex].text;
     var id = subjects[subject];
     table = document.querySelector('.attendance-table');
-    table.classList.remove('hidden');
-    await req(id);
+    // console.log(sub.selectedIndex);
+    if (sub.selectedIndex == 0) {
+        table.classList.add('hidden');
+        document.querySelector('.button-excel').disabled = true;
+
+    } else {
+        table.classList.remove('hidden');
+        req(id);
+        document.querySelector('.button-excel').disabled = false;
+    }
 }
 
+function convert() {
+    let table = document.querySelector(".attendance-table");
+    console.log(table);
+    TableToExcel.convert(table, {
+        name: "Attendance.xlsx"
+    });
+}
 
 $(document).ready(function () {
     $('tbody').scroll(function (e) { //detect a scroll event on the tbody
@@ -159,33 +163,3 @@ $(document).ready(function () {
         $('tbody td:nth-child(2)').css("left", $("tbody").scrollLeft());
     });
 });
-
-
-// req();
-
-
-// const url  = "https://attn-server.herokuapp.com/attn/table/5f206d58ea613a00172d89ff";
-
-// async function req() {       
-//     let req = await fetch(url);
-//     let res = await req.json();
-//     var table = '';
-//     console.log(res.students.length);
-//     var rows = res.students.length;
-//     for(var r = -1; r < rows; r++) {
-//         table += '<tr>';
-//         for(var c = -2 ; c < res.dates.length; c++) {
-//             if(r === -1 && c === -1) table += '<td>' + " " + '</td>';
-//             else if(r === -1 ) {
-//                 var d = new Date(res.dates[c])
-//                 table += '<td>' + d.toLocaleString() + '</td>'
-//             }
-//             else if(c === -2) table += '<td>' + res.students[r].roll + '</td>';
-//             else if(c === -1) table += '<td>' + res.students[r].name + '</td>';
-//             else table += '<td>' + res.students[r].attn[c] + '</td>';
-//         }
-//         table += '</tr>';
-//     }
-//     document.write('<table border=1>' + table + '</table>');
-// }
-// req();
