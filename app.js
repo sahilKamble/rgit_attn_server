052572299742eee96a398dd965cc220774de8957
Genos2000 <paraskamble2000@gmail.com>
2020-08-13 00:58:16 +0530

    localhost https for testing, changed cookies properties and cors options

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var mongoose = require('mongoose');
 const session = require('express-session');
 var passport = require('passport');
 var crypto = require('crypto');
+const app = require("https-localhost")();
+
 
 mongoose.set('useUnifiedTopology', true);
 const url = 'mongodb://localhost:27017/rgitAttn';
@@ -16,7 +18,7 @@ const url = 'mongodb://localhost:27017/rgitAttn';
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 
 connect.then((db) => {
-    console.log("Connected correctly to server");
+  console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
 var indexRouter = require('./routes/index');
@@ -26,9 +28,15 @@ var studentRouter = require('./routes/studentRouter');
 var attnRouter = require('./routes/attnRouter');
 var absRouter = require('./routes/absenteesRouter');
 
-var app = express();
+// var app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'chrome-extension://iijkadhmjmmnpgajhleiailnmfajgkji',
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+app.listen();
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -49,7 +57,10 @@ app.use(session({
   saveUninitialized: true,
   store: sessionStore,
   cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    secure: true,
+    sameSite: 'none',
+    httpOnly: false,
   }
 }));
 
@@ -61,7 +72,7 @@ app.use(passport.session());
 //app.use('/', indexRouter);
 app.get('/', (req, res) => {
   fs.readFile(__dirname + '/public/index.html', 'utf8', (err, text) => {
-      res.send(text);
+    res.send(text);
   });
 });
 
@@ -72,12 +83,12 @@ app.use('/subjects', subjectRouter);
 app.use('/students', studentRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
