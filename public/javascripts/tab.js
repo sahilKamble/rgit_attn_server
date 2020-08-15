c09d7ba37cc4f139e25b6da7a181e46f147e2856
sahilKamble <abg1203@gmail.com>
2020-08-16 01:46:16 +0530

    yesss

--- public/javascripts/tab.js
+++ public/javascripts/tab.js
@@ -1,3 +1,12 @@
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
 async function buildTable(data) {
     var tableHeader = document.querySelector(".table-header");
     var tableBody = document.querySelector(".table-body");
