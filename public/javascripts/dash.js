5f35f05357e7e51858eca2cd26bd3683f206fa6f
sahilKamble <abg1203@gmail.com>
2020-08-16 06:37:10 +0530

    changes in frontend

--- public/javascripts/dash.js
+++ public/javascripts/dash.js
@@ -13,10 +13,38 @@ req.onload = function() {
         for (subject of subjects) {
             console.log(subject.name);
             let entry = document.createElement("a");
-            entry.className = "list-group-item list-group-item-action";
-            entry.innerHTML = subject.name;
+            let tabico = document.createElement("i");
+            let subspan = document.createElement("span");
+            entry.className = "row list-group-item list-group-item-action";
+            subspan.className = "col";
+            tabico.className = "col-1 fas fa-table";
+            subspan.innerHTML = subject.name;
+            //entry.innerHTML = subject.name;
             entry.setAttribute('href', '/users/attn/' + subject._id);
+            entry.appendChild(tabico);
+            entry.appendChild(subspan);
             subjectList.appendChild(entry);
         }
+        let addentry = document.createElement("a");
+        let addico = document.createElement("i");
+        let addspan = document.createElement("span");
+        addentry.className = "row list-group-item list-group-item-action";
+        addspan.className = "col";
+        addico.className = "col-1 fas fa-plus";
+        addspan.innerHTML = "Add another class ...";
+        addentry.setAttribute('href', 'addClass.html');
+        addentry.appendChild(addico);
+        addentry.appendChild(addspan);
+        subjectList.appendChild(addentry);
     }
+}
+
+function logout() {
+    fetch("/users/logout")
+    .then(res => res.json())
+    .then(data => {
+        if(data.success){
+            window.location.href = "/users/login"
+        }
+    })
 }
