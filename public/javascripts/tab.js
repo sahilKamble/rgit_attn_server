4ff301c5af2ffa7ce7be391e91d48a9cf5da2654
Paras Kamble <paraskamble2000@gmail.com>
2020-08-16 17:44:42 +0530

    fix sorting

--- public/javascripts/tab.js
+++ public/javascripts/tab.js
@@ -96,7 +96,7 @@ async function req(sid) {
 
     let students = data.students
         .sort((a, b) => a.roll - b.roll)
-        .sort((a, b) => a.div - b.div);
+        .sort((a, b) => a.div.localeCompare(b.div))
 
     for (student of students) {
         let hmm = {
