df99a80ce75e3cdf5d976f5c04a2e01f8e97b4c2
Paras Kamble <paraskamble2000@gmail.com>
2020-08-18 20:03:56 +0530

    trim username and password

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,16 +1,30 @@
-regform.onsubmit = async (e) => {
-    e.preventDefault();
-    var data = new FormData(regform)
-    var body = {
-        username : data.get("username"),
-        password: data.get("password")
+document.querySelector('#register').addEventListener('click', async function (event) {
+    event.preventDefault();
+    let form = {};
+    
+    let formData = new FormData(document.querySelector('#myForm'));
+    for (let key of formData.keys()) {
+        form[key] = formData.get(key);
     }
-    console.log(body);
-    let response = await fetch('/users/login', {
-      method: 'POST',
-      body: body.toString()
-    });
-
-    let result = await response.json();
-    console.log(result);
-};
+    let httpHeaders = { 'Content-Type': 'application/json' };
+    let url = '/users/register';
+    let myHeaders = new Headers(httpHeaders);
+    let data = {
+        username: form.username.trim(),
+        password: form.password.trim()
+    }
+    let res = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: myHeaders,
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    })
+    let resData = res;
+    console.log(resData.status);
+    if(resData.status != 200) {
+      console.log('error');
+    } else {
+        window.location.href = "/users/login";
+    }
+});
