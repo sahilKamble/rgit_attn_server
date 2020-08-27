38322d974ef96b3422cfb721990640096f5275e0
adrian2504 <60578726+adrian2504@users.noreply.github.com>
2020-08-27 20:10:22 +0530

    changes in add class/paras demag kha raha hai

--- public/javascripts/addClass.js
+++ public/javascripts/addClass.js
@@ -1,73 +1,55 @@
-function showStudents() {
-    let filt = document.getElementById('inputGroupSelect01').value;
-    let N_sub = document.getElementById('subject1').value;
+function showStudents(){
 
-    fetch(`http://localhost:3000/students${filt}`).then((res) => {
-        res.json().then((data) => {
-            console.log(data);
-            if (data.length > 0) {
-                data.sort(function (a, b) {
-                    if (a.div == b.div) {
-                        return a.roll - b.roll;
-                    }
-                });
+fetch("http://localhost:3000/students?div=A").then(
+    res=>{
+        res.json().then(
+            data=>{
+                 
+                console.log(data); 
+                if(data.length>0){
 
-                var temp = '<tr><th>Roll number</th>';
-                temp += '<th>Name</th>';
+                    data.sort(function (a, b) {
+                    return a.roll - b.roll;});
 
-                data.forEach((u) => {
-                    temp += '<tr>';
 
-                    temp +=
-                        '<td>' +
-                        `<input type="checkbox"  name="value" id="c2" value="${u._id}"  checked ><label for="nroll">` +
-                        ' ' +
-                        u.div +
-                        u.roll +
-                        '</label></td>';
-                    temp += '<td>' + u.name + '</td>';
-                });
+                    var temp = "<tr><th>Roll number</th>";
+                     temp += "<th>Name</th>";
 
-                document.getElementById('data').innerHTML = temp;
+                     
+               
+
+                    data.forEach((u)=>{
+                        temp +="<tr>";
+                        
+                        temp +="<td>"+`<input type="checkbox"  name="value" id="c2" value="${u._id}"  checked ><label for="nroll">`+u.roll+"</label></td>";
+                        temp +="<td>"+u.name+"</td>";
+                    })
+                    
+                    document.getElementById("data").innerHTML = temp;
+                }
             }
+        )
+    }
+)
+
+$(document).ready(function () {
+    $("#save-class").click(function () {
+        var sclass = [];
+        $.each($("input[name='value']:checked"), function () {
+            sclass.push($(this).val());
+            
         });
+        alert("Members added are: " + sclass.join(", "));
+
+        console.log(sclass);
     });
+});
 
-    $(document).ready(function () {
-        $('#save-class').click(async function () {
-            var sclass = [];
-            $.each($("input[name='value']:checked"), function () {
-                sclass.push($(this).val());
-            });
-            alert('Members added are: ' + sclass.join(', '));
-
-            console.log(sclass);
-            var obj = {
-                name: N_sub,
-                teacher: '5f37e8b898234f00171a3619',
-                students: sclass,
-            };
-
-            let res = await fetch('/subjects', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(obj),
-            });
-            let resp = await res.json();
-            console.log(resp);
-        });
-    });
 }
 
-async function postClass(data) {
-    let res = await fetch('https://attn-server.herokuapp.com/subjects', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    let resp = await res.json();
-}
+
+  student_id = {
+     "_id": sclass
+     }
+  var stud_id = JSON.stringify(student_id);
+  console.log(stud_id);
