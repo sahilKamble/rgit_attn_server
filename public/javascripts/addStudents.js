b004f61c18e342c544d552e293bd2671455b522c
Genos2000 <paraskamble2000@gmail.com>
2020-09-07 23:11:39 +0530

    fix bug while posting to /students

--- public/javascripts/addStudents.js
+++ public/javascripts/addStudents.js
@@ -1,42 +1,47 @@
 var list = [];
-var d;
+var department;
+
+function ShowAndHide() {
+    var dept = document.querySelector('#dept-dropdown');
+    department = dept.options[dept.selectedIndex].value;
+    console.log(dept.options[dept.selectedIndex].value);
+}
+
 function handleFile(e) {
+    list = [];
     var files = e.target.files,
         f = files[0];
     var reader = new FileReader();
     reader.onload = function (e) {
         var data = new Uint8Array(e.target.result);
         var workbook = XLSX.read(data, { type: 'array' });
-        console.log(workbook.Sheets);
+        // console.log(workbook.Sheets);
 
         let sheetsList = workbook.SheetNames;
         /* DO SOMETHING WITH workbook HERE */
-        var wsj = XLSX.utils.sheet_to_json(workbook.Sheets[sheetsList[0]]);
-        //console.log(wsj);
+        var worksheet = XLSX.utils.sheet_to_json(
+            workbook.Sheets[sheetsList[0]]
+        );
+        console.log(worksheet);
         ShowAndHide();
-        for (student of wsj) {
+        for (student of worksheet) {
             let abc = {
                 name: student.name,
                 roll: student.roll,
                 div: student.div,
-                dept: d,
+                dept: department,
             };
             list.push(abc);
         }
-        console.log(list);
-        // if (list) {
-        //   let request = new XMLHttpRequest();
-        //   request.open("POST", "http://localhost:3000/students", true);
-        //   request.setRequestHeader("Content-Type", "application/json");
-        //   request.send(JSON.stringify(list));
-        // }
+        // console.log(list);
     };
     reader.readAsArrayBuffer(f);
 }
+
 async function addstud() {
     if (list) {
         let request = new XMLHttpRequest();
-        request.open('POST', 'http://localhost:3000/students', true);
+        request.open('POST', '/students', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify(list));
     }
